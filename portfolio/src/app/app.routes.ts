17c9f60fc403dbee8025540ed8ca8ac94cbe41d1
Colby Money <122302338+ColbyMoney@users.noreply.github.com>
2026-07-17 16:04:29 -0500

    remove unnecessary contact info

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -4,6 +4,6 @@ export const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
   { path: 'about', loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent) },
   { path: 'projects', loadComponent: () => import('./pages/projects/projects.component').then(m => m.ProjectsComponent) },
-  { path: 'contact', loadComponent: () => import('./pages/contact/contact.component').then(m => m.ContactComponent) },
+  // { path: 'contact', loadComponent: () => import('./pages/contact/contact.component').then(m => m.ContactComponent) },
   { path: '**', redirectTo: '' }
 ];
